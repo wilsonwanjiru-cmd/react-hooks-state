c6296a72619a76ab44e7ea1b2cf6593c2db55f65
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-08-28 13:26:19 +0300

    <React State>

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,18 @@
-test("Test passing", () => {
-  return new Promise((resolve, reject) => {
-    resolve(true);
-  });
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Counter from "../components/Counter";
+
+test("increments count when button is clicked", () => {
+  const { getByText } = render(<Counter />);
+
+  // Initially, the count should be 0
+  expect(getByText("Count: 0")).toBeInTheDocument();
+
+  // Click the increment button
+  const incrementButton = getByText("Increment");
+  fireEvent.click(incrementButton);
+
+  // After clicking, the count should be 1
+  expect(getByText("Count: 1")).toBeInTheDocument();
 });
+
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
 
 function Counter() {
+  // Initialize state with initial value of 0
   const [count, setCount] = useState(0);
 
+  // Define function to increment count using the callback syntax
   function increment() {
-    setCount(count + 1);
+    // Using callback to ensure we work with the most up-to-date count
+    setCount((currentCount) => currentCount + 1);
   }
 
-  return <button onClick={increment}>I have been clicked {count} times</button>;
+  return (
+    <div>
+      <button onClick={increment}>Increment</button>
+      <p>Count: {count}</p>
+    </div>
+  );
 }
 
 export default Counter;
+
